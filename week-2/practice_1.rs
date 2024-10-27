aaa5bda16614a99e04c0df4389aa521fe5a8c7d8
NjokuWisdomChin <wisdomnjoku411@gmail.com>
2024-10-27 16:22:47 -0700

    (project-2)

---
+++ week-2/practice_1.rs
@@ -0,0 +1,7 @@
+fn main() {
+	println!("Welcome to CSC 101");
+
+	println!("THis course learning outcome is as follows:");
+
+	println!("Welcome to pau");
+}
---
+++ week-2/project-2.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let toshiba:f64 = 450000.0;
+    let mac:f64 = 1500000.0;
+    let hp:f64 = 750000.0;
+    let dell:f64 = 2850000.0;
+    let acer:f64 = 250,000.0;
+
+    let sum = (2*toshiba)+(1*m)+(3*mac)+(3*dell)+(2*acer);
+    println!("Sum total is{}", sum;
+    
+    let totalquality= 2+1+3+3+1;
+    let ave = sum/totalquality;
+	println!("Average {}", ave);
+}
